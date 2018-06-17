241c4967e63c60cfbbf25952035463229b3d1457
Yu Sun <yu.sun.cs@gmail.com>
2018-06-17 09:59:12 -0700

    more samples

--- sample-errors/e9-pair-parentheses/sample1.py
+++ sample-errors/e9-pair-parentheses/sample1.py
@@ -1,2 +1,4 @@
+# missing right p
+
 print (3 + 5)
 print (3 + 9 * (4 + 5)
---
+++ sample-errors/e9-pair-parentheses/sample5.py
@@ -0,0 +1,5 @@
+# extra right parenthesis
+name = input("Please input your name: ")
+age = input("Please input your age: ")
+
+print ("Name: " + name + " Age: " + str(age)))
