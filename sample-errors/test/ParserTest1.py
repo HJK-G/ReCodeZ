560345afc72d29d2f1a443867d3f17f1c083e0b8
Justin Kim <juskim88@gmail.com>
2018-06-17 11:02:10 -0700

    Parser test update with file input

--- sample-errors/test/ParserTest1.py
+++ sample-errors/test/ParserTest1.py
@@ -1,9 +1,11 @@
 import parser
+import sys
 
+f=open("sample1.py","r")
+s=f.readlines()
 try:
-    parser.suite("def asdf(a) print(1)")
-except SyntaxError:
-    print("SOMETHING")
-
-def asdf(a):
-    print(1)
+    parser.suite(s)
+except SyntaxError as se:
+    print(se.lineno)
+    print(se.offset)
+    print(se.msg)
