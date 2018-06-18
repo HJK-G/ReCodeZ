71865d990946519a3296daab4937a7baccf3f625
Justin Kim <juskim88@gmail.com>
2018-06-18 15:27:43 -0700

    Added additional test to try line by line parser.suite()

---
+++ sample-errors/test/ParserTest2.py
@@ -0,0 +1,18 @@
+import parser
+import os
+
+path=os.getcwd()
+os.chdir(path[0:path.index("/test")]+"/e9-pair-parentheses")
+f=open("sample3.py","r")
+fileList=f.readlines()
+
+
+for st in fileList:
+    print(st)
+    try:
+        parser.suite(st)
+    except SyntaxError as se:
+        print(se.lineno)
+        print(se.offset)
+        print(se.msg)
+        print(se.text)
