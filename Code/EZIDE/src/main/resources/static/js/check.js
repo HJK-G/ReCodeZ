92e8adcaf3a5dab06dc04d3f247c1c7cd82c6874
Justin Kim <juskim88@gmail.com>
2018-09-09 12:49:39 -0700

    Made website work with output correct

--- Code/EZIDE/src/main/resources/static/js/check.js
+++ Code/EZIDE/src/main/resources/static/js/check.js
@@ -1,6 +1,6 @@
 function check() {
     var code = $('#code').val();
-    $('#errors').html('<p id="lastresult"></p>')
+    $('#errors').html('<p id="errorIdentifier"></p>')
 
     $.ajax(
         {
@@ -11,9 +11,14 @@ function check() {
             },
             success: function (result) {
                 console.log(result);
-                for (i = 0; i < result.length && result[i] != null; i++) {
-                    $('<div class="card bg-light"><div class="card-header" id="heading' + (i + 1) + '"><h5><button class="btn btn-link text-danger collapsed" type="button" data-toggle="collapse" data-target="#collapse' + (i + 1) + '" aria-expanded="false" aria-controls="collapse' + (i + 1) + '">Error ' + (i + 1) + '</button></h5></div><div id="collapse' + (i + 1) + '" class="collapse" aria-labelledby="heading' + (i + 1) + '" data-parent="#errors"><div class="card-body"><pre class="tab text-danger">' + result[i] + '<pre></div></div></div>').insertBefore("#lastresult");
+                if (result[0] === "0") {
+                	$('<pre class="tab text-success">' + result[1] + '<pre>').insertBefore("#outputIdentifier");
                 }
+                else {
+	               	for (i = 0; i < result.length && result[i] != null; i++) {
+	                    $('<pre class="tab text-danger">' + result[i] + '<pre>').insertBefore("#errorIdentifier");
+	                }
+				}
             },
             error: function (jqXHR, exception) {
                 alert("Failed");
